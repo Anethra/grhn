38a9d029d0e21f96b5af353039a95239574fb5ca
Anethra <grhnozturk@gmail.com>
2022-12-01 23:14:34 +0300

    modifiy

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <Layout>
       <section className={styles.center}>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero accusamus repellat tenetur sapiente corporis dolorem debitis fugiat impedit ut unde modi ullam, natus, laudantium error sed reprehenderit nostrum voluptates inventore deserunt tempora soluta cum illum sint consequatur. Cum, voluptas consectetur molestias similique, earum quis magni consequuntur nisi adipisci iusto nesciunt quae placeat eligendi, repudiandae neque nihil. Voluptatum, reiciendis! Earum vero dolor accusamus officia? Placeat aliquam ab eos autem omnis. Itaque, mollitia, fugit harum vel sint cum ad eius sapiente, non minus reiciendis iste eaque incidunt debitis illum eum nulla. Maiores repellendus quidem sunt sit reprehenderit dolore molestias, laboriosam culpa ad.</p>
+        <p className={styles.major}>To Future </p>
+        <p className={styles.word}>
+          “All our dreams can come true, if we have the courage to pursue them.”
+        </p>
       </section>
     </Layout>
   )
--- pages/pages/about.js
+++ pages/pages/about.js
@@ -1,10 +1,20 @@
 import Layout from '../../components/layout'
 import Styles from '../../styles/home.module.css'
+import Image from 'next/image'
 
 export default function About() {
     return (
         <Layout>
-            <p className={Styles.center}>Hello, I am Gurhan Ozturk. I am born at 1993. Currently working as a Laptop motherboard repair technician. Also I am in the process of learning coding.</p>
+            
+            <div className={Styles.center}>
+                <Image
+                    priority
+                    src="/images/profile.jpg"
+                    height={144}
+                    width={144}
+                />
+                <p>Hello, I am Gurhan Ozturk. I am born at 1993. Currently working as a Laptop motherboard repair technician. Also I am in the process of learning coding.</p>
+            </div>
         </Layout>
     )
 }
